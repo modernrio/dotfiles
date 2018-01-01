32faeba75afc7165f803e02cd1e696bc9497fd2c
Markus Schneider <markus@icet-clan.de>
2018-01-01 11:45:02 +0100

    Update dotdrop.sh

--- dotdrop.sh
+++ dotdrop.sh
@@ -1,29 +1,31 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # author: deadc0de6 (https://github.com/deadc0de6)
 # Copyright (c) 2017, deadc0de6
 
 # check for readlink/realpath presence
 # https://github.com/deadc0de6/dotdrop/issues/6
 rl="readlink -f"
-${rl} ${0} >/dev/null 2>&1
-if [ "$?" != "0" ]; then
+
+if ! ${rl} "${0}" >/dev/null 2>&1; then
   rl="realpath"
-  hash ${rl}
-  [ "$?" != "0" ] && echo "\"${rl}\" not found !" && exit 1
+
+  if ! hash ${rl}; then
+    echo "\"${rl}\" not found !" && exit 1
+  fi
 fi
 
 # setup variables
-args="$@"
-cur=`dirname $(${rl} ${0})`
-opwd=`pwd`
-bin="${cur}/dotdrop/dotdrop/dotdrop.py"
+args=("$@")
+cur=$(dirname "$(${rl} "${0}")")
+opwd=$(pwd)
 cfg="${cur}/config.yaml"
+sub="dotdrop"
 
 # pivot
-cd ${cur}
-# init the submodule
+cd "${cur}" || { echo "Folder \"${cur}\" doesn't exist, aborting." && exit; }
+# init/update the submodule
 git submodule update --init --recursive
 # launch dotdrop
-python3 ${bin} --cfg=${cfg} $args
+PYTHONPATH=dotdrop python3 -m dotdrop.dotdrop --cfg="${cfg}" "${args[@]}"
 # pivot back
-cd ${opwd}
+cd "${opwd}" || { echo "Folder \"${opwd}\" doesn't exist, aborting." && exit; }
